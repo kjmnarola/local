68422722b525402a8663fe5ff799b248f113c7f1
Kenil Modi <mkenil@narola.local>
2021-07-20 17:41:35 +0530

    updates

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Developer's Community</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<script type="text/javascript" src="/js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+ include 'header.php';
+ include 'footer.php';
